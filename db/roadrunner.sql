3367d42c6f1922235940c15b75747f76eb961e14
Elena Vera <vera19elena@gmail.com>
2023-05-11 17:28:12 +0300

    change bschedule table, create booking form,

--- db/roadrunner.sql
+++ db/roadrunner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Φιλοξενητής: 127.0.0.1
--- Χρόνος δημιουργίας: 09 Μάη 2023 στις 12:27:08
+-- Χρόνος δημιουργίας: 11 Μάη 2023 στις 16:18:29
 -- Έκδοση διακομιστή: 10.4.14-MariaDB
 -- Έκδοση PHP: 7.4.11
 
@@ -44,9 +44,7 @@ CREATE TABLE `schedule` (
   `SCHEDULE_ID` varchar(10) NOT NULL,
   `FROM_CITY` varchar(15) NOT NULL,
   `TO_CITY` varchar(15) NOT NULL,
-  `DATE_T` date NOT NULL,
-  `DEPARTURE` time NOT NULL,
-  `ARRIVAL` time NOT NULL,
+  `DATE_T` datetime DEFAULT NULL,
   `SEATS_AVAIL` int(11) NOT NULL,
   `PRICE` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -55,35 +53,35 @@ CREATE TABLE `schedule` (
 -- Άδειασμα δεδομένων του πίνακα `schedule`
 --
 
-INSERT INTO `schedule` (`SCHEDULE_ID`, `FROM_CITY`, `TO_CITY`, `DATE_T`, `DEPARTURE`, `ARRIVAL`, `SEATS_AVAIL`, `PRICE`) VALUES
-('AA101', 'Athens', 'Arta', '2023-06-09', '10:00:00', '13:00:00', 20, 20),
-('AA102', 'Athens', 'Arta', '2023-06-10', '09:00:00', '13:00:00', 30, 20),
-('AP101', 'Athens', 'Patra', '2023-06-06', '09:00:00', '13:00:00', 50, 10),
-('AP102', 'Athens', 'Patra', '2023-06-15', '10:00:00', '14:00:00', 50, 10),
-('AT101', 'Athens', 'Thessaloniki', '2023-06-01', '09:00:00', '15:00:00', 50, 40),
-('AT102', 'Athens', 'Thessaloniki', '2023-06-01', '15:00:00', '21:00:00', 50, 40),
-('AT103', 'Athens', 'Thessaloniki', '2023-06-02', '09:00:00', '15:00:00', 50, 40),
-('AT104', 'Athens', 'Thessaloniki', '2023-06-02', '14:00:00', '20:00:00', 50, 40),
-('AV101', 'Athens', 'Volos', '2023-06-10', '08:00:00', '14:00:00', 40, 30),
-('AV102', 'Athens', 'Volos', '2023-06-08', '10:00:00', '16:00:00', 35, 30),
-('PA101', 'Patra', 'Athens', '2023-06-09', '10:00:00', '14:00:00', 50, 10),
-('PA102', 'Patra', 'Athens', '2023-06-10', '10:30:00', '14:30:00', 50, 10),
-('PAR101', 'Patra', 'Arta', '2023-06-11', '09:00:00', '15:00:00', 30, 10),
-('PAR102', 'Patra', 'Arta', '2023-06-11', '15:00:00', '21:00:00', 30, 10),
-('PT101', 'Patra', 'Thessaloniki', '2023-06-20', '09:00:00', '14:00:00', 40, 30),
-('PT102', 'Patra', 'Thessaloniki', '2023-06-12', '15:00:00', '20:00:00', 40, 30),
-('TA101', 'Thessaloniki', 'Athens', '2023-06-10', '10:00:00', '16:00:00', 50, 40),
-('TA102', 'Thessaloniki', 'Athen', '2023-06-10', '08:30:00', '14:30:00', 50, 40),
-('TA103', 'Thessaloniki', 'Athens', '2023-06-11', '09:00:00', '15:00:00', 50, 40),
-('TA104', 'Thessaloniki', 'Athens', '2023-06-11', '15:00:00', '21:00:00', 50, 40),
-('TP101', 'Thessaloniki', 'Patra', '2023-06-12', '09:00:00', '14:00:00', 30, 30),
-('TP102', 'Thessaloniki', 'Patra', '2023-06-12', '15:00:00', '20:00:00', 30, 30),
-('TV101', 'Thessaloniki', 'Volos', '2023-06-15', '09:00:00', '12:00:00', 20, 10),
-('TV102', 'Thessaloniki', 'Volos', '2023-06-20', '12:00:00', '15:00:00', 20, 10),
-('VA101', 'Volos', 'Athens', '2023-06-18', '10:00:00', '15:00:00', 20, 30),
-('VA102', 'Volos', 'Athens', '2023-06-19', '15:00:00', '20:00:00', 20, 30),
-('VT101', 'Volos', 'Thessaloniki', '2023-06-10', '10:00:00', '13:00:00', 20, 10),
-('VT102', 'Volos', 'Thessaloniki', '2023-06-25', '20:00:00', '23:00:00', 20, 10);
+INSERT INTO `schedule` (`SCHEDULE_ID`, `FROM_CITY`, `TO_CITY`, `DATE_T`, `SEATS_AVAIL`, `PRICE`) VALUES
+('AA101', 'Athens', 'Arta', '2023-06-01 10:00:00', 20, 20),
+('AA102', 'Athens', 'Arta', '2023-06-02 21:30:40', 30, 20),
+('AP101', 'Athens', 'Patra', '2023-06-01 08:00:00', 50, 10),
+('AP102', 'Athens', 'Patra', '2023-06-09 08:00:00', 50, 10),
+('AT101', 'Athens', 'Thessaloniki', '2023-06-01 00:00:00', 50, 40),
+('AT102', 'Athens', 'Thessaloniki', '2023-05-25 00:00:00', 50, 40),
+('AT103', 'Athens', 'Thessaloniki', '2023-05-25 00:00:00', 50, 40),
+('AT104', 'Athens', 'Thessaloniki', '2023-05-13 00:00:00', 50, 40),
+('AV101', 'Athens', 'Volos', '2023-05-20 00:00:00', 40, 30),
+('AV102', 'Athens', 'Volos', '2023-05-22 00:00:00', 35, 30),
+('PA101', 'Patra', 'Athens', '2023-05-24 00:00:00', 50, 10),
+('PA102', 'Patra', 'Athens', '2023-05-16 00:00:00', 50, 10),
+('PAR101', 'Patra', 'Arta', '2023-05-17 00:00:00', 30, 10),
+('PAR102', 'Patra', 'Arta', '2023-05-17 00:00:00', 30, 10),
+('PT101', 'Patra', 'Thessaloniki', '2023-05-31 00:00:00', 40, 30),
+('PT102', 'Patra', 'Thessaloniki', '2023-05-31 00:00:00', 40, 30),
+('TA101', 'Thessaloniki', 'Athens', '2023-06-01 00:00:00', 50, 40),
+('TA102', 'Thessaloniki', 'Athens', '2023-06-01 00:00:00', 50, 40),
+('TA103', 'Thessaloniki', 'Athens', '2023-06-02 00:00:00', 50, 40),
+('TA104', 'Thessaloniki', 'Athens', '2023-06-02 00:00:00', 50, 40),
+('TP101', 'Thessaloniki', 'Patra', '2023-05-18 00:00:00', 30, 30),
+('TP102', 'Thessaloniki', 'Patra', '2023-05-26 00:00:00', 30, 30),
+('TV101', 'Thessaloniki', 'Volos', '2023-05-22 00:00:00', 20, 10),
+('TV102', 'Thessaloniki', 'Volos', '2023-05-25 00:00:00', 20, 10),
+('VA101', 'Volos', 'Athens', '2023-05-12 00:00:00', 20, 30),
+('VA102', 'Volos', 'Athens', '2023-05-24 00:00:00', 20, 30),
+('VT101', 'Volos', 'Thessaloniki', '2023-05-21 00:00:00', 20, 10),
+('VT102', 'Volos', 'Thessaloniki', '2023-05-28 00:00:00', 20, 10);
 
 --
 -- Ευρετήρια για άχρηστους πίνακες
--- server.js
+++ server.js
@@ -25,19 +25,6 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/schedule', (req, res) => {
-  const from = req.query.from;
-  const to = req.query.to;
-  // Retrieve data from the database based on the user's input
-  connection.query(`SELECT FROM_CITY, TO_CITY,DATE_FORMAT(DATE_T, '%c/%e/%Y') AS DATE_T, TIME_FORMAT(DEPARTURE, '%H:%i:%s') AS DEPARTURE, TIME_FORMAT(ARRIVAL, '%H:%i:%s') AS ARRIVAL, SEATS_AVAIL, PRICE FROM schedule WHERE FROM_CITY = ? AND TO_CITY = ?`, [from, to], (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Internal server error');
-    } else {
-      res.render('schedule', { data: results });
-    }
-  });
-});
 app.get('/about', (req, res) => {
   res.render('about');
 });
@@ -49,6 +36,84 @@ app.get('/book', (req, res) => {
 app.get('/home', (req, res) => {
   res.render('home');
 });
+
+app.get('/schedule', (req, res) => {
+  const from = req.query.from;
+  const to = req.query.to;
+  // Retrieve data from the database based on the user's input
+  connection.query(`SELECT FROM_CITY, TO_CITY, DATE_FORMAT(DATE_T, \'%d/%m/%Y %H:%i\') AS DATE_T, SEATS_AVAIL, PRICE FROM schedule WHERE FROM_CITY = ? AND TO_CITY = ?`, [from, to], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    } else {
+      res.render('schedule', { data: results });
+    }
+  });
+});
+
+app.get('/get_to_cities', (req, res) => {
+  const fromCity = req.query.from;
+  const sql = 'SELECT DISTINCT to_city FROM schedule WHERE from_city = ?';
+  connection.query(sql, [fromCity], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/get_dates', (req, res) => {
+  const fromCity = req.query.from;
+  const toCity = req.query.to;
+  const sql = 'SELECT DATE_FORMAT(DATE_T, \'%d/%m/%Y %H:%i\') AS DATE_T FROM schedule WHERE from_city = ? AND to_city = ?';
+
+  connection.query(sql, [fromCity, toCity], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else { 
+      res.json(results);
+    }
+  });
+});
+
+app.post('/book', (req, res) => {
+  const from=req.query.from;
+  const to = req.query.to;
+  const date = req.query.date;
+  const fname = req.query.fname;
+  const lname = req.query.lname;
+
+  // Retrieve the schedule id based on the selected from_city, to_city, and date_t values
+  connection.query(`SELECT SCHEDULE_ID FROM schedule WHERE FROM_CITY = ? AND TO_CITY = ? AND DATE_FORMAT(DATE_T, \'%d/%m/%Y %H:%i\')= ?`, [from, to, date], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    } else {
+
+      // If the query returns no results, return an error
+      if (results.length === 0) {
+        res.status(400).send('Invalid schedule selected');
+        return;
+      }
+
+      const scheduleId = results[0].id;
+
+      // Insert the booking into the database
+      connection.query('INSERT INTO bookings (schedule_id, name, surname) VALUES (?, ?, ?)', [scheduleId, fname, lname], (error, results) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send('Internal server error');
+        } else {
+          res.render('booking_confirmation');
+        }
+      });
+    }
+  });
+});
+
 
 // Start the server
 app.listen(3000, () => {
